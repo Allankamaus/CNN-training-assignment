76cf409b4dae78883303e010566cb8b128f488ea
Allankamaus <allannjoroge.kamau@strathmore.edu>
2025-08-04 15:20:05 +0300

    training code

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ transform = transforms.Compose([
 ])
 
 #DataLoader for dataset
+train_dataset = ImageFolder("ImageFolder", transform=transform)
+val_dataset   = ImageFolder("ImageFolder", transform=transform)
 train_loader = DataLoader(
     train_dataset,batch_size = 32, shuffle = True, num_workers=4
 )
@@ -28,4 +30,14 @@ model = model.resnet50(pretrained=True)
 model.fc = nn.Linear(model.fc.in_features, 10) #Assuming 10 classes
 
 criterion = nn.CrossEntropyLoss()
+Optimizer = optim.Adam(model.parameters(), lr= 0.001)
 
+
+for epoch in range(10): #Number of epochs
+    model.train()  #set model to training mode
+    for images, labels in train_loader:
+        outputs = model(images) #forward pass
+        loss = criterion(outputs, labels)#calculate loss
+        Optimizer.zero_grad() #zero the gradients
+        loss.backward()#backward pass
+        Optimizer.step()
